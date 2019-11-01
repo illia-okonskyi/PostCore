c4a674c03220596a0252c5a5cf2dfd51b99fcea0
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-11-01 15:45:15 +0200

    Added Account view component, updated main layout

---
+++ src/PostCore/PostCore.ViewUtils/ViewComponents/Account.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PostCore.Core.Users;
+
+namespace PostCore.ViewUtils.ViewComponents
+{
+    public class AccountViewModel
+    {
+        public bool IsLoggedIn { get; set; }
+        public User User { get; set; }
+    }
+
+    public class Account : ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+        public Account(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var vm = new AccountViewModel();
+            var userNameMvc = User.Identity.Name;
+            var isLoggedIn = userNameMvc != null;
+            vm.IsLoggedIn = isLoggedIn;
+            if (isLoggedIn)
+            {
+                vm.User = await _userManager.FindByNameAsync(userNameMvc);
+            }
+            return View(vm);
+        }
+    }
+}
