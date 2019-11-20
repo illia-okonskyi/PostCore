b82c7df198a3770704b783be0b22b0d557ad064f
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-11-20 17:16:28 +0200

    Added PagesDivTagHelper tag helper

---
+++ src/PostCore/PostCore.ViewUtils.Tests/TagHelpers/PagesDivTagHelperTest.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using Xunit;
+using PostCore.Utils;
+using PostCore.ViewUtils.TagHelpers;
+
+namespace PostCore.ViewUtils.Tests.TagHelpers
+{
+    public class PagesDivTagHelperTest
+    {
+        static readonly string DefaultListOptionsRouteKey = "options";
+
+        class Context
+        {
+            public IUrlHelperFactory UrlHelperFactory { get; set; }
+            public TagHelperContext TagHelperContext { get; set; }
+            public TagHelperOutput TagHelperOutput { get; set; }
+            public ViewContext ViewContext { get; set; }
+        }
+        Context MakeContext(string controller, string action, string loRouteKey)
+        {
+            var urlHelper = new Mock<IUrlHelper>();
+            urlHelper.Setup(x => x.Action(It.IsAny<UrlActionContext>()))
+                .Returns((UrlActionContext ctx) => {
+                    var routeValues = ctx.Values as Dictionary<string, object>;
+                    ListOptions lo;
+                    if (routeValues.ContainsKey(DefaultListOptionsRouteKey))
+                    {
+                        lo = routeValues[DefaultListOptionsRouteKey] as ListOptions;
+                    }
+                    else
+                    {
+                        lo = routeValues[loRouteKey] as ListOptions;
+                    }
+                    return ctx.Controller + "/" + ctx.Action + "?" + lo.Page;
+                });
+
+            var urlHelperFactory = new Mock<IUrlHelperFactory>();
+            urlHelperFactory.Setup(f => f.GetUrlHelper(It.IsAny<ActionContext>()))
+                .Returns(urlHelper.Object);
+
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                "");
+            var content = new Mock<TagHelperContent>();
+            var tagHelperOutput = new TagHelperOutput(
+                "a",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(content.Object));
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", controller);
+            routeData.Values.Add("action", action);
+            var viewContext = new ViewContext
+            {
+                RouteData = routeData
+            };
+
+            return new Context
+            {
+                UrlHelperFactory = urlHelperFactory.Object,
+                TagHelperContext = tagHelperContext,
+                TagHelperOutput = tagHelperOutput,
+                ViewContext = viewContext
+            };
+        }
+
+        [Fact]
+        public void CheckContent()
+        {
+            var controller = "home";
+            var action = "index";
+            var loRouteKey = "lo";
+            var context = MakeContext(controller, action, loRouteKey);
+
+            // Defaults and only one page
+            var tagHelper = new PagesDivTagHelper(context.UrlHelperFactory)
+            {
+                ViewContext = context.ViewContext,
+                PaginationInfo = new PaginationInfo
+                {
+                    CurrentPage = 1,
+                    TotalPages = 1
+                }
+            };
+
+            tagHelper.Process(context.TagHelperContext, context.TagHelperOutput);
+            var content = context.TagHelperOutput.Content.GetContent();
+            var expectedContent = $@"<a class="" "" href=""{controller}/{action}?1"">&lt;&lt;</a>&#xA;" +
+                $@"<a class=""disabled  "" href=""{controller}/{action}?0"">&lt;</a>&#xA;" +
+                $@"<span class="" "">1</span>&#xA;" +
+                $@"<a class=""disabled  "" href=""{controller}/{action}?2"">&gt;</a>&#xA;" +
+                $@"<a class="" "" href=""{controller}/{action}?1"">&gt;&gt;</a>";
+            Assert.Equal(expectedContent, content);
+
+            // Manual specifying the controller, action, list options route key
+            tagHelper.Controller = controller;
+            tagHelper.Action = action;
+            tagHelper.ListOptionsRouteKey = loRouteKey;
+
+            tagHelper.Process(context.TagHelperContext, context.TagHelperOutput);
+            content = context.TagHelperOutput.Content.GetContent();
+            // expectedContent must be same as previous;
+            Assert.Equal(expectedContent, content);
+
+
+            // Styling and more pages
+            tagHelper.PaginationInfo = new PaginationInfo
+            {
+                CurrentPage = 5,
+                TotalPages = 10,
+            };
+            tagHelper.PageClass = "btn";
+            tagHelper.PageClassNormal = "normal";
+            tagHelper.PageClassCurrent = "current";
+
+            tagHelper.Process(context.TagHelperContext, context.TagHelperOutput);
+            content = context.TagHelperOutput.Content.GetContent();
+            expectedContent = $@"<a class=""normal btn"" href=""{controller}/{action}?1"">&lt;&lt;</a>&#xA;" +
+                $@"<a class=""normal btn"" href=""{controller}/{action}?4"">&lt;</a>&#xA;" +
+                $@"<a class=""normal btn"" href=""{controller}/{action}?3"">3</a>&#xA;" +
+                $@"<a class=""normal btn"" href=""{controller}/{action}?4"">4</a>&#xA;" +
+                $@"<span class=""current btn"">5</span>&#xA;" +
+                $@"<a class=""normal btn"" href=""{controller}/{action}?6"">6</a>&#xA;" +
+                $@"<a class=""normal btn"" href=""{controller}/{action}?7"">7</a>&#xA;" +
+                $@"<a class=""normal btn"" href=""{controller}/{action}?6"">&gt;</a>&#xA;" +
+                $@"<a class=""normal btn"" href=""{controller}/{action}?10"">&gt;&gt;</a>";
+            Assert.Equal(expectedContent, content);
+        }
+    }
+}
---
+++ src/PostCore/PostCore.ViewUtils/TagHelpers/PagesDivTagHelper.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using PostCore.Utils;
+
+namespace PostCore.ViewUtils.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "list-options,pagination-info")]
+    public class PagesDivTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+
+        public PagesDivTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public ListOptions ListOptions { get; set; }
+        public PaginationInfo PaginationInfo { get; set; }
+        public string ListOptionsRouteKey { get; set; } = "options";
+
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassCurrent { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var result = new TagBuilder("div");
+            var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+
+            var minPage = PaginationInfo.CurrentPage - 2;
+            var maxPage = PaginationInfo.CurrentPage + 2;
+            if (minPage < 1)
+            {
+                maxPage = Math.Min(PaginationInfo.TotalPages, maxPage + Math.Abs(minPage) + 1);
+                minPage = 1;
+            }
+            if (maxPage > PaginationInfo.TotalPages)
+            {
+                minPage = Math.Max(1, minPage - Math.Abs(maxPage - PaginationInfo.TotalPages));
+                maxPage = PaginationInfo.TotalPages;
+            }
+
+            result.InnerHtml.AppendHtml(MakePageAnchor(urlHelper, 1, true, "<<"));
+            result.InnerHtml.Append("\n");
+            result.InnerHtml.AppendHtml(MakePageAnchor(
+                urlHelper,
+                PaginationInfo.CurrentPage - 1,
+                PaginationInfo.HasPreviousPage,
+                "<"));
+            result.InnerHtml.Append("\n");
+
+            for (long i = minPage; i <= maxPage; ++i)
+            {
+                if (i == PaginationInfo.CurrentPage)
+                {
+                    result.InnerHtml.AppendHtml(MakeCurrentPageSpan(i));
+                    result.InnerHtml.Append("\n");
+                }
+                else
+                {
+                    result.InnerHtml.AppendHtml(MakePageAnchor(urlHelper, i, true));
+                    result.InnerHtml.Append("\n");
+                }
+            }
+
+            result.InnerHtml.AppendHtml(MakePageAnchor(
+                urlHelper,
+                PaginationInfo.CurrentPage + 1,
+                PaginationInfo.HasNextPage,
+                ">"));
+            result.InnerHtml.Append("\n");
+            result.InnerHtml.AppendHtml(MakePageAnchor(
+                urlHelper,
+                PaginationInfo.TotalPages,
+                true,
+                ">>"));
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.Clear();
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+
+        private IHtmlContent MakeCurrentPageSpan(long page)
+        {
+            var tag = new TagBuilder("span");
+            tag.InnerHtml.Append(page.ToString());
+            tag.AddCssClass(PageClass);
+            tag.AddCssClass(PageClassCurrent);
+            return tag;
+        }
+
+        private IHtmlContent MakePageAnchor(
+            IUrlHelper urlHelper,
+            long page,
+            bool enabled,
+            string displayName = null)
+        {
+            var listOptions = ListOptions ?? new ListOptions();
+            var newListOptions = new ListOptions
+            {
+                Filters = listOptions.Filters,
+                SortKey = listOptions.SortKey,
+                SortOrder = listOptions.SortOrder,
+                Page = page
+            };
+
+            var href = urlHelper.Action(new UrlActionContext
+            {
+                Action = Action ?? ViewContext.RouteData.Values["action"].ToString(),
+                Controller = Controller ?? ViewContext.RouteData.Values["controller"].ToString(),
+                Values = new Dictionary<string, object>
+                {
+                    { ListOptionsRouteKey, newListOptions }
+                }
+            });
+
+            var tag = new TagBuilder("a");
+            tag.InnerHtml.Append(displayName ?? page.ToString());
+            tag.Attributes["href"] = href;
+            tag.AddCssClass(PageClass);
+            tag.AddCssClass(PageClassNormal);
+            if (!enabled)
+            {
+                tag.AddCssClass("disabled");
+            }
+            return tag;
+        }
+    }
+}
