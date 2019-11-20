66b58be9d993b8c12f4dfd17c328bfa8c56cd1fd
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-11-20 17:16:11 +0200

    Added SpanCustomValidationKeyTagHelper tag helper

---
+++ src/PostCore/PostCore.ViewUtils.Tests/TagHelpers/SpanCustomValidationKeyTagHelperTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using Xunit;
+using PostCore.ViewUtils.TagHelpers;
+
+
+namespace PostCore.ViewUtils.Tests.TagHelpers
+{
+    public class SpanCustomValidationKeyTagHelperTest
+    {
+        class Context
+        {
+            public TagHelperContext TagHelperContext { get; set; }
+            public TagHelperOutput TagHelperOutput { get; set; }
+        }
+
+        Context MakeContext()
+        {
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                "");
+            var content = new Mock<TagHelperContent>();
+            var tagHelperOutput = new TagHelperOutput(
+                "a",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(content.Object));
+
+            return new Context
+            {
+                TagHelperContext = tagHelperContext,
+                TagHelperOutput = tagHelperOutput
+            };
+        }
+
+        [Fact]
+        public void CheckContent()
+        {
+            var errorKey = "errorKey";
+            var errorMessage1 = "errorMessage1";
+            var errorMessage2 = "errorMessage2";
+            var errorMessageSeparator = "; ";
+            var viewContext = new ViewContext();
+
+            var context = MakeContext();
+            var tagHelper = new SpanCustomValidationKeyTagHelper
+            {
+                ViewContext = viewContext,
+                ValidationKey = errorKey
+            };
+
+            // No error
+            tagHelper.Process(context.TagHelperContext, context.TagHelperOutput);
+
+            var content = context.TagHelperOutput.Content.GetContent();
+            Assert.True(string.IsNullOrEmpty(content));
+
+            // Has one error
+            viewContext.ModelState.AddModelError(errorKey, errorMessage1);
+            tagHelper.Process(context.TagHelperContext, context.TagHelperOutput);
+
+            content = context.TagHelperOutput.Content.GetContent();
+            Assert.Equal(errorMessage1, content);
+
+            // Has two errors
+            viewContext.ModelState.AddModelError(errorKey, errorMessage2);
+            tagHelper.Process(context.TagHelperContext, context.TagHelperOutput);
+
+            content = context.TagHelperOutput.Content.GetContent();
+            Assert.Equal(errorMessage1 + errorMessageSeparator + errorMessage2, content);
+        }
+    }
+}
---
+++ src/PostCore/PostCore.ViewUtils/TagHelpers/SpanCustomValidationKeyTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace PostCore.ViewUtils.TagHelpers
+{
+    [HtmlTargetElement("span", Attributes = "validation-key")]
+    public class SpanCustomValidationKeyTagHelper : TagHelper
+    {
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public string ValidationKey { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.IsNullOrEmpty(ValidationKey))
+            {
+                return;
+            }
+
+            var modelState = ViewContext.ModelState;
+            if (!modelState.ContainsKey(ValidationKey))
+            {
+                return;
+            }
+            var entry = modelState[ValidationKey];
+            var errorString = string.Join("; ", entry.Errors.Select(e => e.ErrorMessage));
+            output.Content.SetContent(errorString);
+        }
+    }
+
+}
