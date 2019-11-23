310bf97907134095d1a44b71f9f33e9598eebae5
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-11-23 15:13:19 +0200

    Fixed tests for Account ViewComponent

--- src/PostCore/PostCore.ViewUtils.Tests/ViewComponents/AccountTest.cs
+++ src/PostCore/PostCore.ViewUtils.Tests/ViewComponents/AccountTest.cs
@@ -17,8 +17,10 @@ namespace PostCore.ViewUtils.Tests.ViewComponents
     public class AccountTest
     {
         static Account MakeAccountViewComponent(
-            bool hasAuthenticatedUser,
-            User authenticatedUser = null)
+            bool hasAuthenticatedUser = false,
+            User authenticatedUser = null,
+            string path = "/",
+            string query = "?query=query")
         {
             var usersDaoMock = new Mock<IUsersDao>();
             usersDaoMock.Setup(m => m.GetByUserNameAsync(It.IsAny<string>()))
@@ -35,6 +37,10 @@ namespace PostCore.ViewUtils.Tests.ViewComponents
             var httpContextMock = new Mock<HttpContext>();
             httpContextMock.Setup(m => m.User)
                 .Returns(claimsPrincipalMock.Object);
+            httpContextMock.Setup(m => m.Request.Path)
+                .Returns(path);
+            httpContextMock.Setup(m => m.Request.QueryString)
+                .Returns(new QueryString(query));
 
             var viewContext = new ViewContext
             {
@@ -54,12 +60,20 @@ namespace PostCore.ViewUtils.Tests.ViewComponents
         [Fact]
         public async Task NoLoggedInUser()
         {
-            var vc = MakeAccountViewComponent(false);
+            var path = "/path";
+            var query = "?myquery=myquery";
+            var currentUrl = path + query;
+
+            var vc = MakeAccountViewComponent(
+                hasAuthenticatedUser: false,
+                path: path,
+                query: query);
 
             var vm = (await vc.InvokeAsync()).ExtractViewModel<AccountViewModel>();
 
             Assert.False(vm.IsLoggedIn);
             Assert.Null(vm.User);
+            Assert.Equal(currentUrl, vm.CurrentUrl);
         }
 
         [Fact]
@@ -70,14 +84,22 @@ namespace PostCore.ViewUtils.Tests.ViewComponents
                 Id = 1,
                 UserName = "userName"
             };
+            var path = "/path";
+            var query = "?myquery=myquery";
+            var currentUrl = path + query;
 
-            var vc = MakeAccountViewComponent(true, user);
+            var vc = MakeAccountViewComponent(
+                hasAuthenticatedUser: true,
+                authenticatedUser: user,
+                path: path,
+                query: query);
 
             var vm = (await vc.InvokeAsync()).ExtractViewModel<AccountViewModel>();
 
             Assert.True(vm.IsLoggedIn);
             Assert.Equal(user.Id, vm.User.Id);
             Assert.Equal(user.UserName, vm.User.UserName);
+            Assert.Equal(currentUrl, vm.CurrentUrl);
         }
     }
 }
