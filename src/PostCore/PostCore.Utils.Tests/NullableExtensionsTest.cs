5b635fb59a868ba9beca570aa5c20ec3be64b4b3
Illia Okonskyi <illia.okonskyi.work@gmail.com>
2019-11-27 16:16:54 +0200

    Added NullableExtensiosn class

---
+++ src/PostCore/PostCore.Utils.Tests/NullableExtensionsTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace PostCore.Utils.Tests
+{
+    public class NullableExtensionsTest
+    {
+        enum TheEnum
+        {
+            Value1,
+            Value2
+        }
+
+        [Fact]
+        public void TryParse_long()
+        {
+            long value = 123;
+
+            Assert.False(NullableExtensions.TryParse("agfrg", out long? parsed));
+            Assert.False(parsed.HasValue);
+            Assert.True(NullableExtensions.TryParse(value.ToString(), out parsed));
+            Assert.True(parsed.HasValue);
+            Assert.Equal(value, parsed.Value);
+        }
+
+        [Fact]
+        public void TryParse_DateTime()
+        {
+            var value = new DateTime(2019, 11, 26);
+
+            Assert.False(NullableExtensions.TryParse("agfrg", out DateTime? parsed));
+            Assert.False(parsed.HasValue);
+            Assert.True(NullableExtensions.TryParse(value.ToString(), out parsed));
+            Assert.True(parsed.HasValue);
+            Assert.Equal(value, parsed.Value);
+        }
+
+        [Fact]
+        public void TryParse_Enum()
+        {
+            var value = TheEnum.Value1;
+
+            Assert.False(NullableExtensions.TryParse("agfrg", out TheEnum? parsed));
+            Assert.False(parsed.HasValue);
+            Assert.True(NullableExtensions.TryParse(value.ToString(), out parsed));
+            Assert.True(parsed.HasValue);
+            Assert.Equal(value, parsed.Value);
+        }
+
+    }
+}
---
+++ src/PostCore/PostCore.Utils/NullableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PostCore.Utils
+{
+    public static class NullableExtensions
+    {
+        public static bool TryParse(string value, out long? result)
+        {
+            result = long.TryParse(value, out long tempResult) ? tempResult : default(long?);
+            return (result == null) ? false : true;
+        }
+
+        public static bool TryParse(string value, out DateTime? result)
+        {
+            result = DateTime.TryParse(value, out DateTime tempResult) ? tempResult : default(DateTime?);
+            return (result == null) ? false : true;
+        }
+
+        public static bool TryParse<T>(string value, out T? result) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException("Invalid Enum");
+            result = Enum.TryParse(value, out T tempResult) ? tempResult : default(T?);
+            return (result == null) ? false : true;
+        }
+    }
+}
